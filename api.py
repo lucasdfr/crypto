d24ab221639e7043f30d42ac0b775cd94e67a11b
theovig <theo.viggiano@polytechnique.edu>
2022-12-13 14:55:57 +0100

    First commit

---
+++ api.py
@@ -0,0 +1,65 @@
+import requests as re
+import json
+
+API_KEY = 'WWVWQ2KD6JR8GX6DMMPRTXEY6STVG2ZARC'
+BASE_URL = 'https://api.bscscan.com/api?'
+BASE_TEST_URL = 'https://api-testnet.bscscan.com/api?'
+
+TEST = True
+
+
+def call_api(api_arguments):
+    url = BASE_URL
+    url += f"module={api_arguments['module']}"
+    for key in api_arguments:
+        if key != 'module':
+            url += f'&{key}={api_arguments[key]}'
+    url += f'&apikey={API_KEY}'
+    return re.get(url).json()
+
+
+def get_abi(address):
+    api_arguments = {'module': 'contract', 'action': 'getabi', "address": address}
+    abi = call_api(api_arguments)
+    if abi['status'] == '1':
+        return json.loads(abi["result"])
+
+
+def get_account_transactions(address, offset=1, sort='asc', internal=False):
+    """
+    Retourne la balance d'un ou plusieurs comptes
+    :param address:liste de hash ou hash simple
+    :return:
+    """
+    api_arguments = {'module': 'account', "address": address, "startblock": "0",
+                     "endblock": "99999999", "page": "1", "sort": sort}
+    if internal:
+        api_arguments['action'] = 'txlistinternal'
+    else:
+        api_arguments['action'] = 'txlist'
+
+    if offset is not None:
+        api_arguments["offset"] = offset
+
+    return call_api(api_arguments)
+
+
+def get_account_first_and_last_block(address):
+    """
+        Retourne le premier et le dernier block d'un contract
+        :param address:liste de hash ou hash simple
+        :return:
+        """
+    return get_account_transactions(address)['result'][0]['blockNumber'], \
+           get_account_transactions(address, sort='desc')['result'][0]['blockNumber']
+
+
+def get_token_balance(address):
+    """
+    Retourne la balance d'un token
+    :param address:hash
+    :return:
+    """
+    api_arguments = {'module': 'account', 'action': 'balance', "address": address}
+    return call_api(api_arguments)
+
---
+++ walletstats.py
@@ -0,0 +1,138 @@
+from web3 import Web3
+import matplotlib.pyplot as plt
+import pandas as pd
+import json, os
+import numpy as np
+
+from api import get_account_transactions, get_abi
+
+bsc = "https://bsc-dataseed.binance.org/"
+web3 = Web3(Web3.HTTPProvider(bsc))
+
+pd.options.mode.chained_assignment = None  # default='warn'
+
+WALLET_ADDRESS = "0x1884856e3dfe96b143f0ba25bec9f8f1e5feb211"
+COURS_BNB = 290
+
+PANCAKE_SWAP_ROUTER = "0x10ed43c718714eb63d5aa57b78b54704e256024e"
+
+
+def get_transactions_json(address, internal=False):
+    if internal:
+        internal_suffix = 'internal_'
+    else:
+        internal_suffix = ''
+    file_exists = os.path.exists(f'data/wallet_{internal_suffix}{address}.json')
+    if not file_exists:
+        transactions = get_account_transactions(address, offset=None, sort="desc", internal=internal)
+        with open(f'data/wallet_{internal_suffix}{address}.json', 'w') as f:
+            json.dump(transactions, f)
+        return transactions
+    with open(f"data/wallet_{internal_suffix}{address}.json", "r") as wallet_data:
+        return json.load(wallet_data)['result']
+
+
+def get_transactions_df(address, internal=False):
+    df = pd.DataFrame(get_transactions_json(address, internal=internal))
+    df['timeStamp'] = pd.to_datetime(df['timeStamp'], unit='s')
+    return df
+
+
+def transform_df(df, gas=False):
+    df['isError'] = df['isError'].astype('int')
+    df = df[df['isError'] == 0]
+    df['value'] = df['value'].astype('float')
+    df['value'] = df['value'] * 10 ** (-18)
+    df['value'] = np.where(df['from'] == WALLET_ADDRESS,
+                           df['value'] * -1,
+                           df['value'])
+
+    if gas:
+        df['txn_fee'] = df['gasPrice'].astype('float') * df['gasUsed'].astype('float') * 10 ** (-18)
+        df['value'] = df['value'] - df['txn_fee']
+        df.drop(columns=['txn_fee', 'gasPrice', 'gasUsed'])
+    return df.drop(columns=['isError'])
+
+
+def compute_in_out(df, df_internal, title):
+    df = df[['timeStamp', 'from', 'to', 'value', 'isError', 'gasPrice', 'gasUsed']]
+    df_internal = df_internal[['timeStamp', 'from', 'to', 'value', 'isError']]
+    df = transform_df(df, gas=True)
+    df_internal = transform_df(df_internal)
+
+    df = pd.concat([df, df_internal])
+    df = df[['timeStamp', 'value']]
+
+    df = df.groupby([df['timeStamp'].dt.date]).sum(numeric_only=True)
+    df = df.sort_values(by='timeStamp')
+    df['bnb_balance'] = df['value'].cumsum()
+    df.plot.bar(stacked=True, title=title)
+    plt.show()
+    print(f"{title} :", df.head().to_string())
+
+
+def plot_wallet(address, df, df_internal):
+    # What we see on bscscan
+    compute_in_out(df, df_internal, "Historique entrées sorties BSCscan")
+
+    # Compute money received
+    df_money_received = df[(df['methodId'] == '0x') & (df['from'] == address)]
+    money_received = round((df_money_received['value'].astype(float) * 10 ** (-18)).sum(), 2)
+    money_received_dollar = round(money_received * COURS_BNB, 2)
+    df_money_put = df[(df['methodId'] == '0x') & (df['to'] == address)]
+
+    money_put = round((df_money_put['value'].astype(float) * 10 ** (-18)).sum(), 2)
+    money_put_dollar = round(money_put * COURS_BNB, 2)
+    print("Argent retiré du wallet: ", money_received, " BNB or ", money_received_dollar, " $")
+    print("Argent mis dans le wallet: ", money_put, " BNB or ", money_put_dollar, " $")
+
+    # Compute lost/wins without transfers
+    df_win_lose = df[(df['methodId'] != '0x') & ((df['from'] != address) | (df['to'] != address))]
+    df_win_lose_internal = df_internal
+    # print(df_win_lose_internal)
+    compute_in_out(df_win_lose, df_win_lose_internal, "Argent gagné en tradant")
+
+
+def get_transactions_detail(transaction_hash):
+    receipt = web3.eth.get_transaction_receipt(transaction_hash)
+    if len(receipt['logs'])>0:
+        for i in range(0, 100):
+            log = receipt['logs'][i]
+            smart_contract = log["address"]
+            abi = get_abi(log["address"])
+            if abi is not None:
+                contract = web3.eth.contract(smart_contract, abi=abi)
+                transfers = []
+                decoded_logs = list(contract.events.Transfer().processReceipt(receipt))
+                for decoded_log in decoded_logs:
+                    if 'from' in decoded_logs[0]['args']:
+                        transfer = {}
+                        transfer['from'] = decoded_log['args']['from']
+                        transfer['to'] = decoded_log['args']['to']
+                        transfer['value'] = float(decoded_log['args']['value'])
+                        transfers += [transfer]
+                return pd.DataFrame(transfers)
+
+
+def get_holders(df, address):
+    abi = get_abi(address)
+    contract = web3.eth.contract(address=web3.toChecksumAddress(address), abi=abi)
+    df_holders = pd.DataFrame(columns=['HOLDER_HASH', 'WALLET', 'UNIT', 'MONEY_IN', 'TIME_IN', 'MONEY_OUT', 'TIME_OUT'])
+    for index, tx in df.iterrows():
+        if tx['from'] is not None:
+            wallet_balance = web3.eth.getBalance(web3.toChecksumAddress(tx['from']))
+            token_balance = contract.functions.balanceOf(tx['from']).call()
+            print(web3.fromWei(wallet_balance, "ether"))
+            print(web3.fromWei(token_balance, "ether"))
+
+
+def get_tokens_analytics(df):
+    for index, tx in df.iterrows():
+        print(get_transactions_detail(tx['hash']))
+
+
+df = get_transactions_df(WALLET_ADDRESS)
+df_internal = get_transactions_df(WALLET_ADDRESS, internal=True)
+
+get_tokens_analytics(df_internal)
+# print(get_transactions_detail("0x6882f2eb5127b92f987879069fce109d9ae709f5111e7d9713126fef55c48df0"))
